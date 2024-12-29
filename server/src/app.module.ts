cd750160b39fa025a80851998fee96b28ab09c63
Rowan <RowanQ@live.co.uk>
2024-12-29 18:11:57 +0000

    remove example files

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,13 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { EventsModule } from './events/events.module';
 import { BookingsModule } from './bookings/bookings.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [AuthModule, EventsModule, BookingsModule, UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule { }
